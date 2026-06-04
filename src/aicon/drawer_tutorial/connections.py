Gradient now correct

src/aicon/drawer_tutorial/connections.py
@@ -162,7 +162,9 @@ class DistGraspHandConnection(ActiveInterconnection):
     def define_implicit_connection_function(self):
 
         def connection_func(distance_ee_drawer, uncertainty_dist, likelihood_grasped_drawer, gripper_activation, ee_force_mag_meas):
-            print(f"Distance EE-Drawer: {distance_ee_drawer}")
+            likelihood_given_uncertainty = torch.clip(torch.exp(-(uncertainty_dist - 0.2) * 5), max=1.0)
+            innovation_from_uncertainty = likelihood_given_uncertainty - likelihood_grasped_drawer
+            # print(f"Distance EE-Drawer: {distance_ee_drawer}")
             dist_relevance = (1 - torch.sigmoid((distance_ee_drawer[1]-0.5) * 5)) * torch.clip(torch.exp(-(uncertainty_dist - 0.2) * 20), max=1.0)
             expected_dist = torch.sum(distance_ee_drawer) + uncertainty_dist
             likelihood_given_dist = torch.exp(-expected_dist * 4) * dist_relevance
@@ -182,7 +184,7 @@ class DistGraspHandConnection(ActiveInterconnection):
                 likelihood_from_hand_and_force = torch.ones_like(likelihood_given_dist) * self.small_likelihood_value
 
             innovation_from_hand_and_force = likelihood_from_hand_and_force - likelihood_given_dist.detach()
-            return innovation_from_dist + innovation_from_hand_and_force
+            return innovation_from_uncertainty + innovation_from_dist + innovation_from_hand_and_force
 
         return connection_func
 